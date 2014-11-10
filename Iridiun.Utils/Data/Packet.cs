498c429d9a090b532f2d27cfc9274be123db92ec
ValerkaVT <v.tetevin@hotmail.com>
2014-11-10 13:49:42 +0000

    add changes to MasterServer

---
+++ Iridiun.Utils/Data/Packet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Iridiun.Utils.Data
+{
+    [Serializable]
+    public abstract class Packet
+    {
+        public readonly PacketType PacketType;
+        protected Packet(PacketType packetType)
+        {
+            this.PacketType = packetType;
+        }
+    }
+
+    public enum PacketType
+    {
+        ClientPacket,
+        MasterPacket,
+        Ping,
+        Pong
+    }
+}
---
+++ Iridiun.Utils/Data/PacketsFromClientToMaster.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Iridiun.Utils.Data
+{
+    [Serializable]
+    public class Ping : Packet
+    {
+        private int ping;
+
+        public Ping(PacketType packetType, Ping ping) : base(packetType)
+        {}
+    }
+}
