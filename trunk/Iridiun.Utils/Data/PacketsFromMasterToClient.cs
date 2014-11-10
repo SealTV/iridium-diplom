5e5d7d77c15a9e49c5afaf9e1dc687a2ed3afb42
seal.matob@gmail.com <seal.matob@gmail.com@4b7c543f-7b57-8693-8f83-1d1bf6fa16bc>
2014-11-10 13:49:42 +0000

    add changes to MasterServer

---
+++ trunk/Iridiun.Utils/Data/PacketsFromMasterToClient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Iridiun.Utils.Data
+{
+    [Serializable]
+    public class Pong : Packet
+    {
+        private int pong;
+
+        public Pong(PacketType packetType) : base(packetType)
+        {}
+
+    }
+}
