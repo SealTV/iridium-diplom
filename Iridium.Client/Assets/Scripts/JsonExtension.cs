d212f63fc785e955a1215ccb9adc93f9e4d0a768
BaltX666@gmail.com <BaltX666>
2014-12-15 21:41:01 +0000

    Added Many resources, dynamic resource generation

---
+++ Iridium.Client/Assets/Scripts/JsonExtension.cs
@@ -0,0 +1,16 @@
+﻿namespace Assets
+{
+    using System;
+    using System.Globalization;
+    using UnityEngine;
+    using SimpleJSON;
+
+    public static class JsonExtension
+    {
+        public static Vector3 AsVector3(this JSONNode json)
+        {
+            string[] vector = json.Value.Replace(" ", "").Split(';');
+            return new Vector3(float.Parse(vector[0]), -float.Parse(vector[1]));
+        }
+    }
+}
