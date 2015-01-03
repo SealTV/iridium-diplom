fe50a2b336678b5fe3c24fe3a140bd2aace4905a
BaltX666@gmail.com <BaltX666@gmail.com@4b7c543f-7b57-8693-8f83-1d1bf6fa16bc>
2015-01-03 09:47:22 +0000

    Mega commit, psd files for foreachBlock and equalsBlock, added creating sequence from blocks

---
+++ trunk/Iridium.Client/Assets/Scripts/Controller.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    using System.Linq;
+
+    public class Controller : MonoBehaviour
+    {
+
+        private bool isPressed;
+        private Block pressedBlock;
+
+        private Vector3 downPosition;
+        private float screenRatio;
+        // Use this for initialization
+        void Start()
+        {
+            this.screenRatio = 0.5f * Screen.height / Camera.main.orthographicSize;
+            
+        }
+	
+        // Update is called once per frame
+        void Update () {
+            this.TestMouseEvent();
+            if (this.isPressed && this.pressedBlock != null)
+            {
+                this.pressedBlock.transform.position = (Input.mousePosition - this.downPosition) / this.screenRatio;
+            }
+        }
+
+
+        void TestMouseEvent()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                var worldTouch = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                RaycastHit2D[] hits = Physics2D.RaycastAll(new Vector2(worldTouch.x, worldTouch.y), Vector2.zero, Mathf.Infinity);
+                
+                foreach (var hit in hits)
+                {
+                    Transform blockTransform = hit.transform;
+                    if (!blockTransform.name.Contains("Connector"))
+                    {
+                        this.isPressed = true;
+                        this.pressedBlock = blockTransform.GetComponent<SubBlock>().Parent.Parent;
+                        this.downPosition = (Input.mousePosition - this.pressedBlock.transform.position * this.screenRatio);
+                        if (this.pressedBlock.Parent != null)
+                            this.pressedBlock.Parent.Connectors.Remove(this.pressedBlock.ParentConnector);
+                        this.pressedBlock.transform.parent = null;
+                        this.pressedBlock.Parent = null;
+                        break;
+                    }
+                }
+            }
+            if (Input.GetMouseButtonUp(0) && this.isPressed)
+            {
+                this.isPressed = false;
+                Vector2 raycastVector = new Vector2(this.pressedBlock.InputConnector.transform.position.x,
+                                                    this.pressedBlock.InputConnector.transform.position.y);
+                RaycastHit2D[] hits = Physics2D.RaycastAll(raycastVector, Vector2.zero, Mathf.Infinity);
+                foreach (var hit in hits)
+                {
+                    Transform connector = hit.transform;
+                    if (connector.name.Contains("Connector"))
+                    {
+                        Block block = connector.GetComponent<Connector>().Parent;
+                        Debug.Log(block);
+                        if (!block.Connectors.ContainsKey(connector.name))
+                        {
+                            block.Connectors[connector.name] = this.pressedBlock;
+                            this.pressedBlock.Parent = block;
+                            this.pressedBlock.ParentConnector = connector.name;
+                            this.pressedBlock.transform.parent = connector.transform;
+                            this.pressedBlock.transform.position = connector.position;
+                            break;
+                        }
+                    }
+                }
+                this.pressedBlock = null;
+            } 
+        }
+    }
+}
--- trunk/Iridium.Client/Assets/Scripts/SubBlock.cs
+++ trunk/Iridium.Client/Assets/Scripts/SubBlock.cs
@@ -1,40 +1,10 @@
 ﻿namespace Assets.Scripts
 {
-    using System.Collections.Generic;
-    using SimpleJSON;
     using UnityEngine;
 
-    public class SubBlock
+    public class SubBlock : MonoBehaviour
     {
-        public SpriteRenderer Sprite;
-        public GameObject gameObject;
-
-        public bool isHeightStretch;
-        public bool isWidthStretch;
-
-        public bool isHeightShift;
-        public bool isWidthShift;
-
-        public SubBlock(JSONClass json, Transform block)
-        {
-            this.Height = json["height"].AsInt;
-            this.Width = json["width"].AsInt;
-            this.Position = json["position"].AsVector3();
-
-            this.isHeightStretch = json["height_stretch"].AsBool;
-            this.isWidthStretch  = json["width_stretch"] .AsBool;
-
-            this.isHeightShift = json["height_shift"].AsBool;
-            this.isWidthShift  = json["width_shift"].AsBool;
-            
-            this.gameObject = GameObject.Instantiate(Resources.Load(json["object"].Value), this.Position, Quaternion.identity) as GameObject;
-            this.gameObject.transform.parent = block;
-            this.gameObject.transform.localPosition = this.Position;
-        }
-
-        public int Height { get; private set; }
-        public int Width { get; private set; }
-        public Vector3 Position { get; private set; }
+        public SubBlocksField Parent;
     }
 
     
